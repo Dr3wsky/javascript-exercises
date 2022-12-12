232ca90f69be1113491107d2aa8b66129f6f38ea
Andrew McFadden <mcfadden.andrewj@gmail.com>
2022-12-12 13:37:04 -0700

    updated js function and passed tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    const reversedString = [];
+    let i = 0;
+    while (i<=string.length) {
+        reversedString[i] = string.charAt(string.length-i);
+        i++;
+    }
+    return reversedString.join("");
 };
 
 // Do not edit below this line
