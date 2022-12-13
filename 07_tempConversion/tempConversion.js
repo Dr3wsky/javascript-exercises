7f8273d4e33a44beec83d08943346e2499aba991
Andrew McFadden <mcfadden.andrewj@gmail.com>
2022-12-13 08:57:06 -0700

    completed function and passed tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let tempCelcius = (temp - 32) * (5 / 9);
+  return parseFloat(tempCelcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let tempFarenheight = temp *(9/5) +32;
+  return parseFloat(tempFarenheight.toFixed(1));
 };
 
 // Do not edit below this line
