efd0e0b1a4401cadcdc466b78e2022f1b241f643
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-01-17 14:15:12 -0700

    completed exercise and passed unit test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    return array.reduce((a, b) => {
+        const firstAge = getAge(a.yearOfBirth, a.yearOfDeath);
+        const nextAge = getAge(b.yearOfBirth, b.yearOfDeath);
+        return firstAge > nextAge ? a : b;
+    });
+};
 
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death-birth;
 };
 
 // Do not edit below this line
