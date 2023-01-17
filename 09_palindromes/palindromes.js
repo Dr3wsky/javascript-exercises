55baebdce37332131a8dd9b59eced73b0afcf8c2
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-01-16 17:52:39 -0700

    initialized function definition

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    modifiedStr = str.toLowerCase().split();
+    modifiedStr.forEach(element => {
+        
+        if (modifiedstr.charCodeAt(element) < 65 && charCodeAt(element) > 90) {
+            modifiedStr.splice(midifiedStr.indexOf(element, 1));
+        }
+    });
+    reversedStr = modifiedStr.reverse().join('');
+    return reversedStr;
 };
 
 // Do not edit below this line
