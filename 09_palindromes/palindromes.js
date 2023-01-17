0b79d2b0971f2c3299b5563c27ceb0d577405c94
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-01-17 08:45:50 -0700

    completed exercise and passed unit test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,13 +1,14 @@
-const palindromes = function (str) {
-    modifiedStr = str.toLowerCase().split();
-    modifiedStr.forEach(element => {
-        
-        if (modifiedstr.charCodeAt(element) < 65 && charCodeAt(element) > 90) {
-            modifiedStr.splice(midifiedStr.indexOf(element, 1));
-        }
-    });
-    reversedStr = modifiedStr.reverse().join('');
-    return reversedStr;
+const palindromes = function(str) {
+    str = str.toLowerCase();
+    let cleanedStr = [];
+    let reversedStr = [];
+    for (i = 0; i < str.length; i++) {
+        if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) > 91) {
+            cleanedStr.push(str[i]);
+            reversedStr.unshift(str[i]);
+        };
+    };
+    return cleanedStr.join('') === reversedStr.join('');
 };
 
 // Do not edit below this line
